5a68606705f778bc83949d75bed6945dd6966d21
chris westerduin <chriswesterduin@outlook.com>
2021-04-13 12:23:50 +0100

    improve AddAlbum tests

--- src/components/AddAlbum/test.js
+++ src/components/AddAlbum/test.js
@@ -16,6 +16,18 @@ describe("AddAlbum", () => {
     );
   });
 
+  test("updates state of text input with user input", () => {
+    let textInput = screen.getAllByRole("textbox")[0];
+    userEvent.type(textInput, "Testing")
+    expect(textInput.value).toBe("Testing")
+  });
+
+  test("updates state of number input with user input", () => {
+    let numInput = screen.getAllByRole("spinbutton")[0];
+    userEvent.type(numInput, '1234')
+    expect(numInput.value).toBe('1234')
+  });
+
   test("calls the submit function", () => {
     let submitButton = screen.getByRole("button");
     userEvent.click(submitButton)
