febdf9684a5112f420424ea2f873dd79e8d2252c
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-04-13 11:49:37 +0100

    added like button to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,10 @@ function Card(props){
         <h2>{props.data.album}</h2>
         <h3>{props.data.artist}</h3>
         <img src={props.data.image} />
+        
         <small>{props.data.producer} | {props.data.year}</small>
         <Like/>
+        
         </div>
     )
 }
