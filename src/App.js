42270ce0b0ecb62c24b39c74127ad87b20e3d9bc
chris westerduin <chriswesterduin@outlook.com>
2021-04-14 14:10:37 +0100

    try fix router bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Main, Header } from './components'
-import { Home, CardPage } from './pages';
+import { Header } from './components'
+import { Home, CardPage, Albums } from './pages';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -10,7 +10,7 @@ function App(){
         <Header/>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/albums" component={Main} />
+          <Route path="/albums" component={Albums} />
           <Route path="/albums/:id" component={CardPage} />
         </Switch>
       </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as CardPage } from './CardPage';
+export { default as Albums } from './Albums';
 export { default as Error } from './Error';
