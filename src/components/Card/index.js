028c03f45a136527eb261084cf981c83a4d0eaf8
chris westerduin <chriswesterduin@outlook.com>
2021-04-14 12:10:22 +0100

    error for album not found and add id

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Like } from "..";
+import randomColor from './helpers'
 
-function Card({ data }) {
-    const randomColor = () => `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
-    
+function Card({ data, id }) {    
     const [bgColor, setBgColor] = useState(randomColor())
 
     useEffect(() => {
         const init = setInterval(() => setBgColor(randomColor), 3000)
         return (() => clearInterval(init))
     },[])
- 
+ try {
   return (
     <div style={{background: bgColor}} role="listitem">
       <h2>{data.album}<Like /></h2>
@@ -18,9 +17,13 @@ function Card({ data }) {
       <img src={data.image} />
       <small>
         {data.producer} | {data.year}
+        {id && <a href={`/albums/${id}`}>read more...</a>}
       </small>
     </div>
   );
+ } catch (err) {
+   return <h2>album not found...</h2>
+ }
 }
 
 export default Card;
